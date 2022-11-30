2a06a3dc5c301fa252b576ceda1cc8b6b2d6c21a
zeesha44 <shaheeda.nazif@gmail.com>
2022-11-30 05:18:54 -0800

    trefds

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+digit = abs(number) % 10
+if nu(f"Last digit of {number:d} is {digit:d} and is ", end="")
+mber < 0:igit = -digit
+print
